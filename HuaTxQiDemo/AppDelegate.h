13f6d34fbf751b04c58bfac10a147502fc1c79f5
zhongwei <1063163323@qq.com>
2018-04-23 15:39:53 +0800

    Initial Commit

---
+++ HuaTxQiDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HuaTxQiDemo
+//
+//  Created by MiHu on 2018/4/23.
+//  Copyright © 2018年 MiHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuaTxQiDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuaTxQiDemo
+//
+//  Created by MiHu on 2018/4/23.
+//  Copyright © 2018年 MiHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
